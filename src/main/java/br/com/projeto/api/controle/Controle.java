da6a0ac3ef8f5076eecb58dc36673186b57ed028
gtsales <soutoluiz0609@gmail.com>
2023-08-10 11:34:03 -0300

    Iniciando a classe controle

--- src/main/java/br/com/projeto/api/controle/Controle.java
+++ src/main/java/br/com/projeto/api/controle/Controle.java
@@ -1,5 +1,20 @@
 package br.com.projeto.api.controle;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.projeto.api.repositorio.Repositorio;
+
 public class Controle {
     
+    @Autowired
+    private Repositorio acao;
+
+    @GetMapping("/")
+    public String teste(){
+
+        return "Hello World!";
+
+    }
+
 }
